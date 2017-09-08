26297339cd735081192401cbbdbaa7a160f801f7
Belchenkov <u608110@gmail.com>
2017-09-08 21:13:31 +0300

    Setting Nodemailer

--- app.js
+++ app.js
@@ -22,6 +22,56 @@ app.get('/', (req, res) => {
     res.render('contact');
 });
 
+app.post('/send', (req, res) => {
+    const output = `
+            <p>You have a new contact request</p>
+            <h3>Contact Details</h3>
+            <ul>
+                <li>Name: ${req.body.name}</li>
+                <li>Company: ${req.body.company}</li>
+                <li>Email: ${req.body.email}</li>
+                <li>Phone: ${req.body.phone}</li>
+            </ul>
+            <h3>Message</h3>
+            <p>${req.body.message}</p>
+        `;
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'test@test.com', // generated ethereal user
+            pass: 'secret'  // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Nodemailer Contact 👻" <test@test.com>', // sender address
+        to: 'test@gmail.com', // list of receivers
+        subject: 'Node Contact Request ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.render('contact', {msg: 'Email has been sent'});
+    });
+});
+
 app.listen(port, () => {
     console.log('Server Started on localhost:' + port);
 });
