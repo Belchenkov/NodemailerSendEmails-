c099bf2575ff5f4b0f1a164bd58a337d51ceec8c
Belchenkov <u608110@gmail.com>
2017-09-08 20:51:48 +0300

    Create UI Form

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const nodemailer = require('nodemailer');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+// View Engine Setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.render('contact');
 });
 
 app.listen(port, () => {
